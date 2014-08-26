1262e973e8b1092673c8772d598d4d218f87f977
Karthik Ram <karthik.ram@gmail.com>
2014-08-26 14:58:18 -0700

    Added draft of dat init
    - Catching existing dirs is still bad.
    - Error handing in dat is pretty good.

---
+++ R/dat_init.R
@@ -0,0 +1,32 @@
+
+
+#' Initialize a dat repository
+#'
+#' This function will initialize a dat repository
+#' @param path Path to data store. use \code{"."} for current dir.
+#' @export
+#' @keywords initialize
+#' @examples \dontrun{
+#' dat_init('.')
+#'}
+dat_init  <- function(path = ".") {
+	# Needs a better dir tester
+	dir.status <- system(paste("test -d", paste0(path, "/.dat")), intern = FALSE) == 0
+
+	if(!file.exists(path)) {
+	message("Creating new dir? ")	
+	create <- readline(": ")
+	if(create == "Y") {
+	dir.create(path)
+	setwd(path) } 
+
+}
+
+	if(dir.status) {
+	stop("A dat store already exists here") } else {
+		system("dat init")
+	}
+
+}
+
+# Need to be able to pass name, desc, and publisher
